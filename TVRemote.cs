daba4f20bdb402005cc87ac7b1ff8a32686944b1
Paul Standley <paulstandley72@outlook.com>
2019-05-13 00:08:08 +0100

    Interfaces c#

---
+++ TVRemote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTutor
+{
+    class TVRemote
+    {
+        // Now we are modeling the action of
+        // picking up the remote with our hand
+        public static IElectronicDevice GetDevice()
+        {
+            return new Television();
+        }
+    }
+}
